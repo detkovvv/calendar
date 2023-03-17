37eed7fc3cb849d633ed5134f509f8e520f8b6df
Detkov Vasiliy <fgh.detkov@gmail.com>
2023-03-17 17:39:51 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Calendar from './components/calendar';
+
+export default class App extends React.Component {
+    state = {
+        date: null,
+    };
+
+    handleDateChange = date => this.setState({date});
+
+    render() {
+        const {date} = this.state;
+
+        return (
+            <div>
+                {date && <p>Выбранная дата: {date.toLocaleDateString()}</p>}
+                <Calendar
+                    onChange={this.handleDateChange}
+                />
+            </div>
+        )
+    }
+
+}
+
